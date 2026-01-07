1f231910314c10116d0c61166979a567b0c7cada
deorth-kku <deorth@outlook.com>
2026-01-07 23:26:56 +0800

    find length mismatch

--- finderr.py
+++ finderr.py
@@ -4,8 +4,12 @@ from utils import lyrics
 from collections.abc import Generator
 from typing_tube import format_float
 import os
+from tinytag import TinyTag
+from mml import diva_db_file
 
-def has_order_error(it:lyrics)->Generator[str]:
+TOLERANCE=0.2
+
+def haserror(it:lyrics,songlen:float)->Generator[str]:
     last=0
     laststr=""
     for k,v in it:
@@ -15,16 +19,45 @@ def has_order_error(it:lyrics)->Generator[str]:
         laststr=v
     if laststr!="":
         yield "didn't clear text at the end %s"%format_float(last)
+    
+    if last>songlen*(1+TOLERANCE) or last<songlen*(1-TOLERANCE):
+        yield "song length didn't match: lrc:%s, ogg:%s"%(format_float(last),format_float(songlen))
 
+def readogglen(file:str)->float:
+    tag = TinyTag.get(file)
+    duration = tag.duration 
+    return duration
 
+from config import mod_dir,custom_skip
 
+def findogg(input_pv:int)->str:
+    db=dict()
+    for dir in os.listdir(mod_dir):
+        if dir in custom_skip:
+            continue
+        dir=os.path.join(mod_dir,dir)
+        full=os.path.join(dir,r'rom\mod_pv_db.txt')
+        if not os.path.exists(full):
+            continue
+        db:dict[str,str]=diva_db_file(full)
+        db.readlines()
+        filename=db.get("pv_%03d.song_file_name"%input_pv,None)
+        if filename==None:
+            continue
+        filename=os.path.join(dir,filename)
+        if not os.path.exists(filename):
+            continue
+        return filename
+    raise ValueError("cannot find ogg for pv_%03d"%input_pv)
 
 if __name__== "__main__":
     for file in os.listdir(lyrics_outdir):
+        pv_num=int(file.split("_")[0])
         file=os.path.join(lyrics_outdir,file)
         it=readtoml(file)
+        ogglen=readogglen(findogg(pv_num))
         try:
-            errs=list(has_order_error(it))
+            errs=list(haserror(it,ogglen))
         except Exception as e:
             print("%s has format error %s"%(file,e))
             continue
---
+++ findoffset.py
@@ -0,0 +1,10 @@
+
+
+from utils import find_offset
+import sys
+
+
+if __name__ == "__main__":
+    print(find_offset(int(sys.argv[1])))
+
+    
